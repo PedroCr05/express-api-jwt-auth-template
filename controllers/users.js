8ed31a1f122644e0fa27dbcae489430d57a186ec
pedrocruz <JayJMarquez05@gmail.com>
2024-11-12 13:44:04 -0600

    I dont know what's going on. Just copying and pasting now sadly

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const express = require(`express`);
 const router = express.Router();
 const bcrypt = require(`bcrypt`);
 const User = require(`../models/user`);
+const jwt = require("jsonwebtoken");
 
 const SALT_LENGTH = 12;
 
@@ -25,9 +26,13 @@ router.post("/signin", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
     if (user && bcrypt.compareSync(req.body.password, user.hashedPassword)) {
-      res.json({ message: "You are authorized!" });
+      const token = jwt.sign(
+        { username: user.username, _id: user._id },
+        process.env.JWT_SECRET
+      );
+      res.status(200).json({ token });
     } else {
-      res.json({ message: "Invalid credentials." });
+      res.status(401).json({ error: "Invalid username or password." });
     }
   } catch (error) {
     res.status(400).json({ error: error.message });

67dcd550f7f1ad1ee967698ee35cf8012defdee6
pedrocruz <JayJMarquez05@gmail.com>
2024-11-12 12:37:50 -0600

    Getting an error?

--- controllers/test-jwt.js
+++ controllers/test-jwt.js
@@ -1,8 +1,13 @@
 const express = require(`express`);
 const router = express.Router();
+const jwt = require(`jsonwebtoken`);
 
 router.get(`/sign-token`, (req, res) => {
   const testUser = { _id: 1, undername: `test`, password: `test` };
 });
+
+const token = jwt.sign({ testUser }, process.env.JWT_SECRET);
+
+res.json({ token });
 
 module.exports = router;

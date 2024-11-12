8eb0300429d56b465209f1be8782b0963c7e79ef
pedrocruz <JayJMarquez05@gmail.com>
2024-11-12 13:45:49 -0600

    I guess I catched up...

--- controllers/users.js
+++ controllers/users.js
@@ -22,18 +22,21 @@ router.post(`/signup`, async (req, res) => {
   }
 });
 
-router.post("/signin", async (req, res) => {
+router.post("/signup", async (req, res) => {
   try {
-    const user = await User.findOne({ username: req.body.username });
-    if (user && bcrypt.compareSync(req.body.password, user.hashedPassword)) {
-      const token = jwt.sign(
-        { username: user.username, _id: user._id },
-        process.env.JWT_SECRET
-      );
-      res.status(200).json({ token });
-    } else {
-      res.status(401).json({ error: "Invalid username or password." });
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (userInDatabase) {
+      return res.json({ error: "Username already taken." });
     }
+    const user = await User.create({
+      username: req.body.username,
+      hashedPassword: bcrypt.hashSync(req.body.password, SALT_LENGTH),
+    });
+    const token = jwt.sign(
+      { username: user.username, _id: user._id },
+      process.env.JWT_SECRET
+    );
+    res.status(201).json({ user, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }

67456ccfa3e45f47b40cba70d93afc2f6284f826
pedrocruz <JayJMarquez05@gmail.com>
2024-11-12 12:39:25 -0600

    Nevermind, had to put the sign method into the scope of user.

--- controllers/test-jwt.js
+++ controllers/test-jwt.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const jwt = require(`jsonwebtoken`);
 
 router.get(`/sign-token`, (req, res) => {
-  const testUser = { _id: 1, undername: `test`, password: `test` };
+  const user = { _id: 1, undername: `test`, password: `test` };
+
+  const token = jwt.sign({ user }, process.env.JWT_SECRET);
+
+  res.json({ token });
 });
-
-const token = jwt.sign({ testUser }, process.env.JWT_SECRET);
-
-res.json({ token });
 
 module.exports = router;
